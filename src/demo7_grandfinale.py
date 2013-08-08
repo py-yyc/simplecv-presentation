1b7163f68cc824596a7618a6e51568d4e598ff04
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-08-08 01:01:10 -0600

    Found the face first and then found the mustache within it; used the nose instead of the mouth to locate the mustache area.

--- src/demo7_grandfinale.py
+++ src/demo7_grandfinale.py
@@ -6,18 +6,28 @@ cam = SimpleCV.Camera()
 disp = SimpleCV.Display(settings.DISPLAY_SIZE)
 
 stache = SimpleCV.Image(join('..', 'media', 'stash.png'))
-STACHE_SCALE = 0.8
+STACHE_SCALE = 1.2
+NOSE_HEIGHT_FACTOR = 2.0/3
+
+face_cascade = SimpleCV.HaarCascade('face.xml')
+nose_cascade = SimpleCV.HaarCascade('nose.xml')
 
 while disp.isNotDone():
     img = cam.getImage().scale(0.3)
 
-    mouthes = img.findHaarFeatures('mouth.xml')
-    if mouthes is not None:
-        mouth = mouthes.sortArea()[-1]
-        resized_stache = stache.resize(int(mouth.width() * STACHE_SCALE))
-        stache_x = mouth.points[0][0] + (mouth.width()/2) - (resized_stache.width/2)
-        stache_y = mouth.points[0][1]
-        img = img.blit(resized_stache, pos=(stache_x, stache_y), mask=resized_stache.invert())
+    faces = img.findHaarFeatures('face.xml')
+    if faces is not None:
+        face = faces.sortArea()[-1]
+        cropped_face = face.crop()
+        noses = cropped_face.findHaarFeatures('nose.xml')
+        if noses is not None:
+            nose = noses.sortArea()[-1]
+            resized_stache = stache.resize(int(nose.width() * STACHE_SCALE))
+            face_x, face_y = face.x - (face.width()/2), face.y - (face.height()/2)
+            nose_x, nose_y = nose.x - (nose.width()/2), nose.y - (nose.width()/2)
+            stache_x = face_x + nose_x + (nose.width()/2) - (resized_stache.width/2)
+            stache_y = face_y + nose_y + int(nose.height()*NOSE_HEIGHT_FACTOR)
+            img = img.blit(resized_stache, pos=(stache_x, stache_y), mask=resized_stache.invert())
     img.save(disp)
 
     if disp.lastLeftButton:
