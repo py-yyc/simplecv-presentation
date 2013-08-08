40fc409d353a2451e91346b3079860301e56f61e
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-08-07 23:16:41 -0600

    scaled the image down; choose the mouth by largest area

--- src/demo7_grandfinale.py
+++ src/demo7_grandfinale.py
@@ -9,11 +9,11 @@ stache = SimpleCV.Image(join('..', 'media', 'stash.png'))
 STACHE_SCALE = 0.8
 
 while disp.isNotDone():
-    img = cam.getImage()
+    img = cam.getImage().scale(0.3)
 
     mouthes = img.findHaarFeatures('mouth.xml')
     if mouthes is not None:
-        mouth = mouthes[0]
+        mouth = mouthes.sortArea()[-1]
         resized_stache = stache.resize(int(mouth.width() * STACHE_SCALE))
         stache_x = mouth.points[0][0] + (mouth.width()/2) - (resized_stache.width/2)
         stache_y = mouth.points[0][1]
