c2b89376aec656abfe87b795709e751f655ba7d6
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-08-07 21:52:11 -0600

    cleaned up animated gif demo

--- src/demo2_animatedgif.py
+++ src/demo2_animatedgif.py
@@ -8,9 +8,13 @@ disp = SimpleCV.Display(settings.DISPLAY_SIZE)
 
 imset = SimpleCV.ImageSet()
 
+TIME_LIMIT = 4.0
+STEP = 0.2
+TOTAL_STEPS = TIME_LIMIT / STEP
+
 i = 0
 while disp.isNotDone():
-    time.sleep(0.2)
+    time.sleep(STEP)
 
     img = cam.getImage()
     imset.append(img)
@@ -20,7 +24,7 @@ while disp.isNotDone():
         break
 
     i += 1
-    if i >= 20:
+    if i >= TOTAL_STEPS:
         break
 
 imset.save(join('..', 'media', 'animated.gif'))
